72cc71d7e7187ba3f986a8613e42010a3c2783f4
cindittle <cindy.ung.298@my.csun.edu>
2024-12-08 19:12:12 -0800

    12th commit

--- welcome.php
+++ welcome.php
@@ -47,6 +47,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
     <link rel="stylesheet" href="welcome.css">
+    <script src="welcome.js" defer></script>
 </head>
 <body>
 
